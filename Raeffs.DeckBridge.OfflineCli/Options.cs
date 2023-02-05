55c03784e95831661b7c7c579fb4e7425c0ca5b5
Raphael Fleischlin <git@raeffs.dev>
2023-02-05 16:58:05 +0100

    add offline cli

---
+++ Raeffs.DeckBridge.OfflineCli/Options.cs
@@ -0,0 +1,25 @@
+﻿using CommandLine;
+using Raeffs.DeckBridge.Common;
+
+namespace Raeffs.DeckBridge.OfflineCli;
+
+internal record Options
+{
+    [Option('i', "inFile", Required = true, HelpText = "Convert the specified file.")]
+    public string InputFile { get; init; } = string.Empty;
+
+    [Option('o', "outFile", HelpText = "Write the output to the specified file. If not specified, the output will be written to standard output.")]
+    public string OutputFile { get; init; } = string.Empty;
+
+    [Option("force", HelpText = "Overwrite the output file if it already exists.")]
+    public bool Force { get; init; }
+
+    [Option('f', "outFormat", Default = DeckWriterProvider.Generic, HelpText = "Specify the output format.")]
+    public DeckWriterProvider OutputFormat { get; init; }
+
+    [Option("delverLensDataFile", Default = "dlens.db")]
+    public string DelverLensDataFile { get; init; } = "dlens.db";
+
+    [Option("scryfallBulkDataFile", Default = "scryfall.json")]
+    public string ScryfallBulkDataFile { get; init; } = "scryfall.json";
+}
---
+++ Raeffs.DeckBridge.OfflineCli/Program.cs
@@ -0,0 +1,98 @@
+﻿using CommandLine;
+using CommandLine.Text;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Raeffs.DeckBridge.Common;
+using Raeffs.DeckBridge.Deckstats;
+using Raeffs.DeckBridge.DelverLens;
+using Raeffs.DeckBridge.Generic;
+using Raeffs.DeckBridge.OfflineCli;
+using Raeffs.DeckBridge.Scryfall;
+
+var parser = new Parser();
+var result = parser.ParseArguments<Options>(args);
+
+if (result.Errors.Any())
+{
+    var helpText = HelpText.AutoBuild(result);
+    helpText.AddEnumValuesToHelpText = true;
+    helpText.AddOptions(result);
+    await Console.Error.WriteAsync(helpText);
+    Environment.Exit(1);
+}
+
+try
+{
+    var options = result.Value;
+
+    if (!File.Exists(options.ScryfallBulkDataFile))
+    {
+        await Console.Error.WriteLineAsync($"The scryfall bulk data file '{options.ScryfallBulkDataFile}' does not exist or cannot be accessed!");
+        Environment.Exit(1);
+    }
+
+    if (!File.Exists(options.DelverLensDataFile))
+    {
+        await Console.Error.WriteLineAsync($"The delver lens data file '{options.DelverLensDataFile}' does not exist or cannot be accessed!");
+        Environment.Exit(1);
+    }
+
+    if (!File.Exists(options.InputFile))
+    {
+        await Console.Error.WriteLineAsync($"The file '{options.InputFile}' does not exist or cannot be accessed!");
+        Environment.Exit(1);
+    }
+
+    var outputToFile = !string.IsNullOrWhiteSpace(options.OutputFile);
+    if (outputToFile && File.Exists(options.OutputFile) && !options.Force)
+    {
+        await Console.Error.WriteLineAsync($"The file '{options.OutputFile}' does already exist!");
+        Environment.Exit(1);
+    }
+
+    var builder = Host.CreateApplicationBuilder();
+
+    var config = new ConfigurationBuilder()
+        .AddInMemoryCollection(new Dictionary<string, string?>
+        {
+            { "Scryfall:BulkDataFile", options.ScryfallBulkDataFile },
+            { "DelverLens:DataFile", options.DelverLensDataFile }
+        })
+        .Build();
+
+    builder.Services
+        .AddGeneric()
+        .AddDelverLens(config.GetSection("DelverLens"))
+        .AddScryfall(config.GetSection("Scryfall"))
+        .AddDeckstats();
+
+    builder.Services
+        .AddTransient<DeckWriterSelector>();
+
+    var host = builder.Build();
+
+    var initializer = host.Services.GetRequiredService<IAppInitializer>();
+    await initializer.InitializeAsync();
+
+    var reader = host.Services.GetRequiredService<IDeckReader<Card>>();
+    var writerSelector = host.Services.GetRequiredService<DeckWriterSelector>();
+    var writer = writerSelector.SelectWriter(options.OutputFormat);
+
+    if (outputToFile)
+    {
+        await using var outStream = new FileStream(options.OutputFile, FileMode.Create, FileAccess.Write, FileShare.None);
+        await writer.WriteDeckAsync(outStream, reader.ReadDeckAsync(options.InputFile));
+    }
+    else
+    {
+        await using var outStream = Console.OpenStandardOutput();
+        await writer.WriteDeckAsync(outStream, reader.ReadDeckAsync(options.InputFile));
+    }
+}
+catch (Exception exception)
+{
+    await Console.Error.WriteLineAsync("An unexpected error occurred!");
+    await Console.Error.WriteLineAsync(exception.ToString());
+    Environment.Exit(1);
+}
