39935c83f846f41a496d8c1aba21ed3cb7a16c2c
Raphael Fleischlin <git@raeffs.dev>
2023-02-06 21:39:29 +0100

    extract csv deck writer

---
+++ Raeffs.DeckBridge.Csv/CsvDeckWriter.cs
@@ -0,0 +1,33 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using Raeffs.DeckBridge.Common;
+using System.Globalization;
+
+namespace Raeffs.DeckBridge.Csv;
+
+public abstract class CsvDeckWriter<TMap> : IDeckWriter
+    where TMap : ClassMap<Card>
+{
+    private readonly CsvConfiguration _configuration = new(CultureInfo.InvariantCulture);
+
+    public abstract DeckWriterProvider ProviderName { get; }
+
+    public async Task WriteDeckAsync(Stream stream, IAsyncEnumerable<Card> cards, CancellationToken cancellationToken = default)
+    {
+        await using var writer = new StreamWriter(stream);
+        await using var csv = new CsvWriter(writer, _configuration);
+
+        csv.Context.RegisterClassMap<TMap>();
+
+        csv.WriteHeader<Card>();
+        await csv.NextRecordAsync().ConfigureAwait(false);
+
+        await foreach (var card in cards.WithCancellation(cancellationToken).ConfigureAwait(false))
+        {
+            csv.WriteRecord(card);
+            await csv.NextRecordAsync().ConfigureAwait(false);
+        }
+
+        await csv.FlushAsync().ConfigureAwait(false);
+    }
+}
--- Raeffs.DeckBridge.Deckstats/DeckstatsDeckWriter.cs
+++ Raeffs.DeckBridge.Deckstats/DeckstatsDeckWriter.cs
@@ -1,35 +1,9 @@
-﻿using CsvHelper;
-using CsvHelper.Configuration;
-using Raeffs.DeckBridge.Common;
-using System.Globalization;
+﻿using Raeffs.DeckBridge.Common;
+using Raeffs.DeckBridge.Csv;
 
 namespace Raeffs.DeckBridge.Deckstats;
 
-internal class DeckstatsDeckWriter : IDeckWriter
+internal class DeckstatsDeckWriter : CsvDeckWriter<DeckstatsCardMap>
 {
-    private readonly CsvConfiguration _configuration = new(CultureInfo.InvariantCulture)
-    {
-
-    };
-
-    public DeckWriterProvider ProviderName => DeckWriterProvider.Deckstats;
-
-    public async Task WriteDeckAsync(Stream stream, IAsyncEnumerable<Card> cards, CancellationToken cancellationToken = default)
-    {
-        await using var writer = new StreamWriter(stream);
-        await using var csv = new CsvWriter(writer, _configuration);
-
-        csv.Context.RegisterClassMap<DeckstatsCardMap>();
-
-        csv.WriteHeader<Card>();
-        await csv.NextRecordAsync().ConfigureAwait(false);
-
-        await foreach (var card in cards.WithCancellation(cancellationToken).ConfigureAwait(false))
-        {
-            csv.WriteRecord(card);
-            await csv.NextRecordAsync().ConfigureAwait(false);
-        }
-
-        await csv.FlushAsync().ConfigureAwait(false);
-    }
+    public override DeckWriterProvider ProviderName => DeckWriterProvider.Deckstats;
 }
